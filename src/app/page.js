068a31b1a40dc7923e492f8e45f959a8a1fd7082
Skoruz Technologies <itsupport@skoruztek.onmicrosoft.com>
2025-05-31 10:18:09 +0530

    CI/CD env change

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>Welcome to the Home Page</h1>;
+  return (
+    <div>
+       <h1>Welcome to the Home Page</h1>;
+       <h5>////\\\\</h5>
+    </div>
+  )
 }
